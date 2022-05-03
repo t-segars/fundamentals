30ffff1a8b6acf7cad7ccfe8197d9a58171e2565
Tim <98896176+twistedstached@users.noreply.github.com>
2022-05-03 16:13:37 -0700

    third

---
+++ test.py
@@ -0,0 +1,50 @@
+class BankAccount:
+    def __init__(self, int_rate, balance):
+        self.int_rate = int_rate
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+        elif self.balance < amount:
+            print("Insufficient funds: Charging a $5 fee")
+            self.balance -= 5
+        return self
+
+    def display_account_info(self):
+        return(f"Balance: ${self.balance}")
+
+    def yield_interest(self):
+        self.balance = self.balance * self.int_rate + self.balance
+        return self
+
+class User:
+    def __init__(self,name,email):
+        self.name = name
+        self.email = email
+        self.account = BankAccount (int_rate = 0.02, balance = 0)
+
+    def transfer_money(self, amount, other_user):
+        self.account.balance = self.account.balance - amount
+        other_user.account.balance = other_user.account.balance + amount 
+        print(f"{self.name} just transferred {amount} dollars to {other_user.name}")
+        return self
+
+    def display_account_info(self):
+        print(f"Name: {self.name}, {self.account.display_account_info()}")
+        return self
+
+frog = User("Frog","frog@gmail.com")
+pig = User("Pig","pig@yahoo.com")
+bear = User("Bear","bear@hotmail.com")
+
+frog.account.deposit(300)
+frog.display_account_info()
+pig.account.deposit(250)
+pig.display_account_info()
+pig.transfer_money(300,frog)
+bear.account.deposit(500).withdraw(75).display_account_info()
