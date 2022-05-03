e82a932b79db46da84c0e5373a18fbcc66e80c41
Tim <98896176+twistedstached@users.noreply.github.com>
2022-05-03 16:13:20 -0700

    third

--- bankAccount.py
+++ bankAccount.py
@@ -15,25 +15,37 @@ class BankAccount:
         return self
 
     def display_account_info(self):
-        print(f"Account Interest Rate: $ {self.int_rate}")
-        print(f"Account Balance: $ {self.balance}")
-        print("******************************")
-        return self
+        return(f"Balance: ${self.balance}")
 
     def yield_interest(self):
         self.balance = self.balance * self.int_rate + self.balance
         return self
 
-    @classmethod
-    def print_all_accounts(cls):
-        for account in cls.accounts:
-            account.display_account_info()
-        return cls
+class User:
+    def __init__(self,name,email):
+        self.name = name
+        self.email = email
+        self.account = BankAccount (int_rate = 0.02, balance = 0)
+
+    def transfer_money(self, amount, other_user):
+        self.account.balance = self.account.balance - amount
+        other_user.account.balance = other_user.account.balance + amount 
+        print(f"{self.name} just transferred {amount} dollars to {other_user.name}")
+        return self
+
+    def display_account_info(self):
+        print(f"Name: {self.name}, {self.account.display_account_info()}")
+        return self
 
 checking = BankAccount(0.03)
 savings =  BankAccount(0.07)
+frog = User("Frog","frog@gmail.com")
+pig = User("Pig","pig@yahoo.com")
+bear = User("Bear","bear@hotmail.com")
 
-checking.deposit(50).deposit(100).deposit(150).withdraw(150).yield_interest()
-savings.deposit(15000).deposit(5000).withdraw(1000).withdraw(2500).withdraw(750).withdraw(220).yield_interest()
-
-BankAccount.print_all_accounts()
+frog.account.deposit(300)
+frog.display_account_info()
+pig.account.deposit(250)
+pig.display_account_info()
+pig.transfer_money(300,frog)
+bear.account.deposit(500).withdraw(75).display_account_info()
